58e8299ed785aa3f1dd3bbb6fd7703f6ddef6640
smtv62 <fckfacebook@hotmail.com.tr>
2025-12-25 23:00:43 +0300

    Update channels.py

--- channels.py
+++ channels.py
@@ -1,5 +1,6 @@
 CHANNELS = [
-    {"name": "Bein Sports 1 HD", "file": "index.m3u8"},
-    {"name": "Bein Sports 2 HD", "file": "index.m3u8"},
-    {"name": "S Sport HD", "file": "index.m3u8"},
+    {
+        "name": "S Sport HD",
+        "file": "playlist.m3u8"
+    }
 ]
